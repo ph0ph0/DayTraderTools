f27ef21879afcbd74e9c07fe865234b95f4e9cb2
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-07-27 13:06:39 +0100

    just about to now add the new fonts

--- components/VendingMachine/vendingMachineWindow/VendingMachineWindow.js
+++ components/VendingMachine/vendingMachineWindow/VendingMachineWindow.js
@@ -1,41 +1,70 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import { motion, useAnimation, useMotionValue } from "framer-motion";
+import { Center, Textarea } from "@chakra-ui/react";
 
 const VendingMachineWindow = ({ ...props }) => {
   const x = useMotionValue(500);
 
-  const y = true;
+  const isOpen = true;
 
-  return <Window animate={{ height: y ? "100px" : "10px" }} />;
+  return (
+    <WindowHolder>
+      <Window
+        animate={{
+          height: isOpen ? "10px" : "200px",
+          transition: { duration: 3, ease: "anticipate" },
+        }}
+      />
+      <ResultBucket resize={"none"} disabled>
+        Hey there!
+      </ResultBucket>
+    </WindowHolder>
+  );
 };
 
-const Circle = styled.div`
-  height: 100px;
-  width: 100px;
-  border-style: solid;
-  border-width: 5px;
-  border-radius: 50%;
-  border-color: black;
-  cursor: grab;
-`;
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  height: 450px;
-`;
-
-const Window = styled(motion.div)`
-  width: 200px;
+const WindowHolder = styled(Center)`
+  margin-top: 75px;
   height: 200px;
-  border-color: red;
-  border-width: 2px;
-  background-color: green;
+  /* background-color: blue; */
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
 `;
 
-// const WindowWrapper = styled(Window)``;
+// #d9e7fc
+const Window = styled(motion.div)`
+  margin-top: 0px;
+  width: 250px;
+  height: 200px;
+  border-radius: 5px;
+  border-color: #acc6f2;
+  background-color: #acc6f2;
+  border-bottom-color: #9da5b3;
+  border-bottom-width: thin;
+  border-width: 12px;
+  z-index: 2;
+  /* visibility: hidden; */
+`;
+
+const ResultBucket = styled(Textarea)`
+  margin-top: 0px;
+  width: 250px;
+  height: 200px;
+  background-color: #889ec2;
+  position: absolute;
+  z-index: 1;
+  border-radius: 5px;
+  border-width: 7px;
+  border-color: #363e4d;
+  outline: none;
+  overflow: auto;
+  &:hover {
+    border-color: #363e4d;
+  }
+  padding-top: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+`;
 
 const VendingMachineWindowWrapper = styled(VendingMachineWindow)``;
 
