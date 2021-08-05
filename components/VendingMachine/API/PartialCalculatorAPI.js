237e4c2144f262db5362e47c9143af66de651b6c
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-08-05 21:26:05 +0100

    added CORS, now trying to get the polling function to work

--- components/VendingMachine/API/PartialCalculatorAPI.js
+++ components/VendingMachine/API/PartialCalculatorAPI.js
@@ -1,6 +1,5 @@
-// import { useEndpoint } from "../../../CustomHooks/useEndpoint";
-import { useState } from "react";
 import axios from "axios";
+import { poll, validatePollingResponse } from "../helpers/PollingFunctions";
 
 const PartialCalculatorAPI = ({ state, setState }) => {
   const rValues = state.rValues;
@@ -33,8 +32,10 @@ const PartialCalculatorAPI = ({ state, setState }) => {
     // Lambda request receiver endpoint
     const partialCalculatorURL =
       "https://fecu0p7sjj.execute-api.eu-west-2.amazonaws.com/test/partialoptimiserlambda";
+    const pollingURL =
+      "https://fecu0p7sjj.execute-api.eu-west-2.amazonaws.com/test/pollingresource";
 
-    const data = {
+    const dataInputs = {
       id: "1010",
       R: [2, 4],
       P_AtR: [80, 57],
@@ -44,17 +45,49 @@ const PartialCalculatorAPI = ({ state, setState }) => {
     const headers = {
       "Content-Type": "application/json",
     };
-    window.log(`Sending data to endpoint: ${JSON.stringify(data)}`);
+    window.log(`Sending data to endpoint: ${JSON.stringify(dataInputs)}`);
+    // Send data to cloud
+    var queryID;
     try {
       setLoading(true);
-      const response = await axios.post(partialCalculatorURL, data, headers);
+      const response = await axios.post(
+        partialCalculatorURL,
+        dataInputs,
+        headers
+      );
       window.log(`Response: ${JSON.stringify(response)}`);
+      const body = JSON.parse(response["data"]["body"]);
+      window.log(`body: ${JSON.stringify(body)}`);
+      queryID = body["id"];
+      if (dataInputs.id != queryID) {
+        throw "database id is not the same as sent id";
+      }
+      window.log(`Done sending data to backend`);
       setLoading(false);
     } catch (error) {
       setLoading(false);
       setError(error);
       window.log(`Error hitting API: ${error}`);
+      return;
     }
+    window.log(`Now looking to poll with id: ${queryID}`);
+    // Now poll the backend until we get a response.
+
+    const queryBackend = async () => {
+      await axios.post(pollingURL, queryID, headers);
+    };
+
+    try {
+      const partialCalculatorResult = await poll(
+        queryBackend,
+        validatePollingResponse,
+        15
+      );
+    } catch (error) {
+      window.log(`Error polling: ${error}`);
+    }
+
+    window.log(`PartialCalculatorResult: ${partialCalculatorResult}`);
 
     setState((prevState) => {
       return {
@@ -97,6 +130,8 @@ const PartialCalculatorAPI = ({ state, setState }) => {
     rValues,
     probabilities,
     windowIsOpen,
+    loading,
+    error,
     updateRValues,
     updateProbabilities,
     submitData,
---
+++ components/VendingMachine/helpers/PollingFunctions.js
@@ -0,0 +1,36 @@
+// Polling functions
+export const poll = async (fn, validate, maxAttempts) => {
+  window.log(`Starting poll...`);
+  let attempts = 0;
+  let interval;
+
+  const executePoll = async (resolve, reject) => {
+    let result;
+    interval = attempts * 500;
+    try {
+      result = await fn();
+      window.log(`Result from poll: ${result}`);
+    } catch (error) {
+      window.log(`Error running polling query ${error}`);
+      return reject(error);
+    }
+
+    attempts++;
+    window.log(`Attempt: ${attempts}, Interval: ${interval}`);
+
+    if (validate(result)) {
+      return resolve(result);
+    } else if (maxAttempts == attempts) {
+      return reject(new Error("Failed to get result from polling"));
+    } else {
+      setTimeout(executePoll, interval, resolve, reject);
+    }
+  };
+
+  return new Promise(executePoll);
+};
+
+export const validatePollingResponse = (response) => {
+  if (response === undefined) return false;
+  return true;
+};
